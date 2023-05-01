5d8615cf5599925b92535ac809c104968ddeeaad
david-pilny <93579404+david-pilny@users.noreply.github.com>
2023-05-01 15:36:09 +0200

    third commit

--- hook/fetchNews.js
+++ hook/fetchNews.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const fetchNews = (date) => {
-  const apiKey = '<rapid api key>'
+  const apiKey = '<key>'
   const [data, setData] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
